4f72df59fdd3d38ec40b461b8dca77f2fc32c737
Richard Großer <richard.grosser@thulb.uni-jena.de>
2017-02-09 12:24:09 +0100

    Preparition for unit tests

---
+++ tests/unit-tests/View/Helper/AbstractViewHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ThULBTest\View\Helper;
+
+/**
+ * Description of AbstractViewHelperTest
+ *
+ * @author Richard Großer <richard.grosser@thulb.uni-jena.de>
+ */
+abstract class AbstractViewHelperTest extends \VuFindTest\Unit\ViewHelperTestCase
+{
+    //put your code here
+}
---
+++ tests/unit-tests/View/Helper/Record/MetaDataHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ThULBTest\View\Helper\Record;
+
+use ThULB\RecordDriver\SolrVZGRecord;
+use ThULB\View\Helper\Record\MetaDataHelper;
+
+/**
+ * All unit tests for the MetaDataHelper 
+ *
+ * @author Richard Großer <richard.grosser@thulb.uni-jena.de>
+ */
+class MetaDataHelperTest extends \ThULBTest\View\Helper\AbstractViewHelperTest {
+    // test dummy to check working phpunit mechanism     
+    public function testFoo()
+    {
+        return true;
+    }
+}
