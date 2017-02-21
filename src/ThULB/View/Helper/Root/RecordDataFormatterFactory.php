d1a9ed39e1511bb4a111f99c77c3ad2a605f3383
Richard Großer <richard.grosser@thulb.uni-jena.de>
2017-02-21 12:43:16 +0100

    Changed view helper usage to be as close as possible to the new vufind cores behaviour; adjusted test dummy (refs #120, refs #97)

--- src/ThULB/View/Helper/Root/RecordDataFormatterFactory.php
+++ src/ThULB/View/Helper/Root/RecordDataFormatterFactory.php
@@ -3,47 +3,21 @@
  * Factory for record driver data formatting view helper
  *
  * PHP version 5
- *
- * Copyright (C) Villanova University 2016.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @category VuFind
- * @package  View_Helpers
- * @author   Demian Katz <demian.katz@villanova.edu>
+ * 
  * @author   Richard Großer <richard.grosser@thulb.uni-jena.de>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development:architecture:record_data_formatter
- * Wiki
  */
 namespace ThULB\View\Helper\Root;
 
 use VuFind\View\Helper\Root\RecordDataFormatter;
 use VuFind\View\Helper\Root\RecordDataFormatter\SpecBuilder;
+use VuFind\View\Helper\Root\RecordDataFormatterFactory as OrignalFactory;
 
 /**
  * Factory for record driver data formatting view helper
  *
- * @category VuFind
- * @package  View_Helpers
- * @author   Demian Katz <demian.katz@villanova.edu>
  * @author   Richard Großer <richard.grosser@thulb.uni-jena.de>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://vufind.org/wiki/development:architecture:record_data_formatter
- * Wiki
  */
-class RecordDataFormatterFactory
+class RecordDataFormatterFactory extends OrignalFactory
 {
     /**
      * Create the helper.
@@ -52,18 +26,18 @@ class RecordDataFormatterFactory
      */
     public function __invoke()
     {
-        $helper = new RecordDataFormatter();
-        $helper->setDefaults('core', $this->getDefaultCoreSpecs());
-        $helper->setDefaults('description', $this->getDefaultDescriptionSpecs());
+        $helper = parent::__invoke();
+        $helper->setDefaults('full', $this->getDefaultFullSpecs());
+        
         return $helper;
     }
-
+    
     /**
-     * Get default specifications for displaying data in core metadata.
+     * Get default specifications for displaying data in full metadata.
      *
      * @return array
      */
-    public function getDefaultCoreSpecs()
+    public function getDefaultFullSpecs()
     {
         $spec = new SpecBuilder();
         $spec->setTemplateLine(
@@ -131,24 +105,11 @@ class RecordDataFormatterFactory
             'Related Items', 'getAllRecordLinks', 'data-allRecordLinks.phtml'
         );
         $spec->setTemplateLine('Tags', true, 'data-tags.phtml');
-        return $spec->getArray();
-    }
-
-    /**
-     * Get default specifications for displaying data in the description tab.
-     *
-     * @return array
-     */
-    public function getDefaultDescriptionSpecs()
-    {
-        $spec = new SpecBuilder();
         $spec->setLine('Summary', 'getSummary');
-        $spec->setLine('Published', 'getDateSpan');
         $spec->setLine('Item Description', 'getGeneralNotes');
         $spec->setLine('Physical Description', 'getPhysicalDescriptions');
         $spec->setLine('Publication Frequency', 'getPublicationFrequency');
         $spec->setLine('Playing Time', 'getPlayingTimes');
-        $spec->setLine('Format', 'getSystemDetails');
         $spec->setLine('Audience', 'getTargetAudienceNotes');
         $spec->setLine('Awards', 'getAwards');
         $spec->setLine('Production Credits', 'getProductionCredits');
@@ -156,7 +117,6 @@ class RecordDataFormatterFactory
         $spec->setLine('ISBN', 'getISBNs');
         $spec->setLine('ISSN', 'getISSNs');
         $spec->setLine('DOI', 'getCleanDOI');
-        $spec->setLine('Related Items', 'getRelationshipNotes');
         $spec->setLine('Access', 'getAccessRestrictions');
         $spec->setLine('Finding Aid', 'getFindingAids');
         $spec->setLine('Publication_Place', 'getHierarchicalPlaceNames');
