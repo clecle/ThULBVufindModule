059a6a23a31c61c9597469281e1b4ddd15aba4b9
Richard Großer <richard.grosser@thulb.uni-jena.de>
2017-03-02 16:21:50 +0100

    Added test for basic classification (refs #97 refs)

---
+++ tests/unit-tests/src/ThULBTest/View/Helper/Root/BasisClassificationMetadataTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ThULBTest\View\Helper\Root;
+
+/**
+ * Description of BasisClassificationMetadataTest
+ *
+ * @author Richard Großer <richard.grosser@thulb.uni-jena.de>
+ */
+class BasisClassificationMetadataTest extends AbstractRecordDataFormatterTest
+{
+    protected $sheetName = 'Basisklassifikation';
+    protected $metadataKey = 'Basic Classification';
+    protected $template = 'data-basicClassification.phtml';
+            
+}
