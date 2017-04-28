6d899cf3cbc06a171f2f39db8e688835ef5e0149
clecle <clemens.kynast@uni-jena.de>
2017-04-28 11:56:17 +0200

    series statement as title - if title not set. now we have title-information in short-lists (search-list, related volumes/articles)

--- src/ThULB/RecordDriver/SolrVZGRecord.php
+++ src/ThULB/RecordDriver/SolrVZGRecord.php
@@ -42,9 +42,13 @@ class SolrVZGRecord extends \VuFind\RecordDriver\SolrMarc
     
     public function getTitle()
     {
-      return isset($this->fields['title']) ?
+      $title = isset($this->fields['title']) ?
         is_array($this->fields['title']) ?
         $this->fields['title'][0] : $this->fields['title'] : '';
+      if ($title == '') {
+        $title = $this->getFirstFieldValue('490', ['a']);
+      }
+      return $title;
     }
     
     /**
