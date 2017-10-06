9ea8f4b859e969a31028acf7d48f0fec985c0e8c
Richard Großer <richard.grosser@thulb.uni-jena.de>
2017-10-06 10:43:55 +0200

    Different style for development system (refs #282)

--- src/ThULB/View/Helper/Root/ServerType.php
+++ src/ThULB/View/Helper/Root/ServerType.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace ThULB\View\Helper\Root;
 
 use Zend\View\Helper\AbstractHelper;
 
 /**
- * Description of ServerType
+ * Helper class to provide the VUFIND_ENV superglobal content
  *
  * @author Richard Großer <richard.grosser@thulb.uni-jena.de>
  */
-class ServerType extends AbstractHelper {
+class ServerType extends AbstractHelper
+{
     /**
      * Get the VuFind environment variable value.
      *
