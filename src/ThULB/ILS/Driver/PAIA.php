b5c3ad6fe0713477c7399a2f5047a0da8c579908
Richard Großer <richard.grosser@thulb.uni-jena.de>
2018-02-05 11:46:34 +0100

    Show recall links for items that are recallable, even if they are not available without duedate (fixes #321)

--- src/ThULB/ILS/Driver/PAIA.php
+++ src/ThULB/ILS/Driver/PAIA.php
@@ -318,7 +318,10 @@ class PAIA extends OriginalPAIA
     {
         $status = parent::getItemStatus($item);
         
-        if (!$status['availability'] && !isset($status['duedate'])) {
+        if (!$status['availability'] 
+            && !isset($status['duedate'])
+            && $status['holdtype'] !== 'recall'
+        ) {
             $status['use_unknown_message'] = true;
         }
         
