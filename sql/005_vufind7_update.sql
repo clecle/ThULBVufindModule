3a0efee1d8a4b50be22ba1d4b431c5d63027d408
Tobias Groth <tobiasgroth@uni-jena.de>
2020-07-29 13:41:00 +0200

    Updated db update script for vufind7.

--- sql/005_vufind7_update.sql
+++ sql/005_vufind7_update.sql
@@ -1,1 +1,2 @@
-alter table `shortlinks` add `hash` varchar(32);
+ALTER TABLE `shortlinks` ADD `hash` varchar(32);
+ALTER TABLE `shortlinks` ADD UNIQUE KEY `shortlinks_hash_IDX` USING HASH (`hash`);
